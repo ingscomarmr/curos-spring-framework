0663a9c86f9a876bdf7334f650898f2c4daf05a1
Omar Munguia Riv <ingscomarmr@gmail.com>
2023-08-31 19:26:36 -0600

    C18 Pasar objeto a Model desde controlador
    C18 Pasar objeto a Model desde controlador: Se agrega metodo perfil al controlador y se le pasa un objeto para para que se pongan datos en view

--- spring-mvc-01/src/main/java/com/comr/cursos/spring/mvc/controllers/InicioController.java
+++ spring-mvc-01/src/main/java/com/comr/cursos/spring/mvc/controllers/InicioController.java
@@ -3,6 +3,9 @@ package com.comr.cursos.spring.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.comr.cursos.spring.mvc.models.Usuario;
+
 
 //para declarar controlador hay que poner notación @Controller
 @Controller
@@ -10,8 +13,19 @@ public class InicioController {
 	
 	//@RequestMapping(value = {"/index","","/home"},method = RequestMethod.GET) forma 1
 	@GetMapping({"/index","/home","/"}) //la forma mas simple para mapeo
-	public String Index(Model model) { //model es para pasar atributos
+	public String index(Model model) { //model es para pasar atributos
 		model.addAttribute("title", "Esto es una app de Spring FrameWork"); //se agrega el atriburo
 		return "index";//el nombre de la vista
+	}
+	
+	@RequestMapping("/perfil") //por default es metodo Get
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Omar");
+		usuario.setApellido("Munguia Rivera");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Datos ".concat(usuario.getNombre())); //se agrega el atriburo
+		
+		return "perfil";
 	}
 }
