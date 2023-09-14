9a93c0c25145587f2a9657e87ab6639cc92e1f92
Omar Munguia Riv <ingscomarmr@gmail.com>
2023-09-14 17:52:24 -0600

    C21 ModelAttribute objetos genericos
    C21 ModelAttribute objetos genericos para acceder de manera rapida a objetos en la view

--- spring-mvc-01/src/main/java/com/comr/cursos/spring/mvc/controllers/InicioController.java
+++ spring-mvc-01/src/main/java/com/comr/cursos/spring/mvc/controllers/InicioController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.comr.cursos.spring.mvc.models.Usuario;
 
@@ -36,9 +37,9 @@ public class InicioController {
 	
 	@RequestMapping("/listarusuarios") //por default es metodo Get
 	public String listarUsuarios(Model model) {
-		
-		
-		List<Usuario> lista = new ArrayList<>();
+				
+		//SE PASAN DE MANERA GLOBAL @ModelAttribute("usuarios")
+		/*List<Usuario> lista = new ArrayList<>();
 		lista.add(new Usuario("Eder", "Neri", "Eder@correo.com"));
 		lista.add(new Usuario("Jon", "Sepa", "Jon.com"));
 		lista.add(new Usuario("Richi", "Richon", "Richi@correo.com"));
@@ -47,13 +48,23 @@ public class InicioController {
 		usuario.setNombre("Omar");
 		usuario.setApellido("Munguia Rivera");
 		usuario.setEmail("micorre@correo.com");
-		lista.add(usuario);
+		lista.add(usuario);*/
 		
 		//atributos
 		model.addAttribute("titulo", "LISTA USUARIOS"); //se agrega el atriburo
-		model.addAttribute("usuarios", lista);
-		
-		
+		//model.addAttribute("usuarios", lista); ya no se manda el objeto porque desde el ModelAttribute se esta enviando
+				
 		return "listarusuarios";
+	}
+	
+	//Este metodo esta accesible para todo el controlador, si hace referencia alguna vista a usuarios este metodo sera el que responda
+	@ModelAttribute("usuarios")
+	public List<Usuario> getUsuarios(){ //retorna el objeto
+		List<Usuario> lista = new ArrayList<>();
+		lista.add(new Usuario("Eder", "Neri", "Eder@correo.com"));
+		lista.add(new Usuario("Jon", "Sepa", "Jon.com"));
+		lista.add(new Usuario("Richi", "Richon", "Richi@correo.com"));
+		
+		return lista;
 	}
 }
